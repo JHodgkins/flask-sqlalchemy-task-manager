2d5655668d9299231e4fd4ca1bfb108acaf8a70e
JHodgkins <jhodgkins78@gmail.com>
2022-06-25 15:06:27 +0000

    accordian collapsable function added for task views

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -14,4 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // Category select field initialization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+  // Collapsible task view initialization  
+    collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
   });
