55069c91e402c1059527655eefe66e6472462853
JHodgkins <jhodgkins78@gmail.com>
2022-06-25 12:38:25 +0000

    date picker and select field variables and options added

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,17 @@
-// Navbar initialization
+
 document.addEventListener('DOMContentLoaded', function() {
+  // Navbar initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+  // Date picker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm yyyy",
+      i18: {done: "Select"}
+    });
+
+  // Category select field initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
